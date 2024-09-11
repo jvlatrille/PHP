b84ce7a6823c8e7c3a3add6e613926de23408f9d
Jules VINET LATRILLE <jvlatrille@iutbayonne.univ-pau.fr>
2024-09-11 16:26:42 +0200

    Exo 3 + Rien mais fichier Exo 4

---
+++ TP1/Ex3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP1 - Ex3</title>
+</head>
+
+<body>
+    <?php
+    $file = fopen('restos.csv', 'r');
+    
+    while (($line = fgets($file)) !== false) {
+        $data = explode(';', $line);
+    
+        $nom = ($data[0]);
+        $prenom = ($data[1]);
+        $restaurant = ($data[2]);
+
+        echo "Nom : $nom <br> Prénom : $prenom <br> Restaurant : $restaurant <br> <hr>";
+    }
+
+    fclose($file);
+    ?>
+
+</body>
+
+</html>
---
+++ TP1/Ex4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP1 - Ex4</title>
+</head>
+
+<body>
+    <?php
+    // On désire réaliser un compteur de visites dans une page d’accueil.
+    // Simple avec ouverture 2 fois du fichier contenant le compteur
+    $compteur = fopen('compteur.txt', 'r+');
+
+    ?>
+
+</body>
+
+</html>
