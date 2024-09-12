7569fafa22e5a4b6f629d8cd9e0a49f7c7c51822
Jules VINET LATRILLE <jvlatrille@iutbayonne.univ-pau.fr>
2024-09-12 12:03:38 +0200

    Exo 1 fait

---
+++ TP2/Ex1form.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_GET["nom"]) && isset($_GET["age"]) && isset($_GET["mail"]) && isset($_GET["don"])) {
+    $nom = $_GET["nom"];
+    $age = $_GET["age"];
+    $mail = $_GET["mail"];
+    $don = $_GET["don"];
+
+    $fichier = fopen("Ex1resultat.txt", "a");
+
+    fwrite($fichier, "Nom : " . $nom . " | ");
+    fwrite($fichier, "Age : " . $age . " | ");
+    fwrite($fichier, "Mail : " . $mail . " | ");
+    fwrite($fichier, "Don : " . $don . " | \n");
+
+    fclose($fichier);
+    header("Location: Ex1.html");
+    exit;
+}
+
+?>
